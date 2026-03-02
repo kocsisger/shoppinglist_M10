a9604fba288fe2d9e482e1038dfd587bb78d0209
Dr. John H. Watson <john.watson@inf.unideb.hu>
2026-03-02 11:18:29 +0100

    ActivityResultLauncher works

--- app/src/main/java/hu/unideb/inf/shoppinglist/ItemsActivity.java
+++ app/src/main/java/hu/unideb/inf/shoppinglist/ItemsActivity.java
@@ -1,7 +1,9 @@
 package hu.unideb.inf.shoppinglist;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -24,6 +26,9 @@ public class ItemsActivity extends AppCompatActivity {
     }
 
     public void handleItemAdd(View view) {
+        Intent resultIntent = new Intent();
+        resultIntent.putExtra("ITEM", ((Button)view).getText().toString());
+        setResult(RESULT_OK, resultIntent);
         finish();
     }
 }
--- app/src/main/java/hu/unideb/inf/shoppinglist/MainActivity.java
+++ app/src/main/java/hu/unideb/inf/shoppinglist/MainActivity.java
@@ -2,15 +2,29 @@ package hu.unideb.inf.shoppinglist;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityResultLauncher activityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            activityResult -> {
+                Log.d("ITEMS_TEST", "I have returned");
+                String item = activityResult.getData().getStringExtra("ITEM");
+                Log.d("ITEMS_TEST", item);
+            }
+    );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +40,6 @@ public class MainActivity extends AppCompatActivity {
 
     public void handleAddButton(View view) {
         Intent intent = new Intent(this, ItemsActivity.class);
-        startActivity(intent);
+        activityResultLauncher.launch(intent);
     }
 }
