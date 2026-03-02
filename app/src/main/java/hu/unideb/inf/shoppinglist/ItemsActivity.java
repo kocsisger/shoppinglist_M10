0cf0a0021c6edda8c43c29cd43c718daa633b72d
Dr. John H. Watson <john.watson@inf.unideb.hu>
2026-03-02 10:39:54 +0100

    New activity opens

--- app/src/main/java/hu/unideb/inf/shoppinglist/ItemsActivity.java
+++ app/src/main/java/hu/unideb/inf/shoppinglist/ItemsActivity.java
@@ -1,8 +1,6 @@
 package hu.unideb.inf.shoppinglist;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,22 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class ItemsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_items);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-
-    public void handleAddButton(View view) {
-        Intent intent = new Intent(this, ItemsActivity.class);
-        startActivity(intent);
     }
 }
